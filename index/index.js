2a60019f0187bf1506d870e7630e3ebf6efff8d5
Shima Masaeli <shimamasaeli91@gmail.com>
2023-12-04 20:30:13 +0330

    Pages are responsive now.

--- index/index.js
+++ index/index.js
@@ -5,105 +5,6 @@ document.addEventListener("DOMContentLoaded", () => {
   if (!token) {
     window.location.href = "./login.html";
   }
-  document.addEventListener("DOMContentLoaded", () => {
-    const loginForm = document.getElementById("login-form");
-
-    const token = localStorage.getItem("token");
-
-    // Check if the token exists in local storage
-    if (token && token !== "undefined") {
-      // If the token exists, redirect to the index.html
-      window.location.href = "./index.html";
-    }
-
-    loginForm.addEventListener("submit", async (event) => {
-      event.preventDefault();
-
-      const email = document.getElementById("email-address").value;
-      const password = document.getElementById("password").value;
-
-      try {
-        const response = await fetch(
-          "https://dev-api-when-time-tracker.iplugx.ir/api/auth/login",
-          {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-              Accept: "application/json",
-            },
-            body: JSON.stringify({ email, password }),
-          }
-        );
-
-        // Check if the login was successful
-        if (response.ok) {
-          const data = await response.json();
-
-          const token = data.token;
-
-          // Store the token in local storage
-          localStorage.setItem("token", token);
-          // Store the email address regardless of login success or failure
-          localStorage.setItem("userEmail", email);
-
-          // Display the success message for 5 seconds then redirect to index.html
-          const successMessageElement =
-            document.getElementById("success-message");
-          showAndHideMessage(successMessageElement, 4);
-
-          setTimeout(() => {
-            window.location.href = "./index.html";
-          }, 3000);
-        } else {
-          const result = await response.json();
-          let errorMessage = result.detail;
-          console.log(errorMessage);
-
-          const userFriendlyErrorMessage = {
-            undefined: "Your email or password is wrong. Please try again.",
-          };
-          const failureMessageElement =
-            document.getElementById("failure-message");
-          document.getElementById("failure-message").innerText =
-            userFriendlyErrorMessage[errorMessage] ||
-            "Tou are not registered. Please register first.";
-          showAndHideMessage(failureMessageElement, 4);
-          resetAllFields();
-        }
-      } catch (error) {
-        // Get the specific error message
-        const errorMessage = error.message;
-        console.log(errorMessage);
-
-        // Translate the error message to a more user-friendly format
-        const userFriendlyErrorMessage = {
-          network_error:
-            "We couldn't connect to the server. Please check your internet connection and try again.",
-          // Other error messages
-        };
-
-        // Display the error message in the unexpected error div
-        document.getElementById("Unexpected-error").innerText =
-          userFriendlyErrorMessage[errorMessage] ||
-          "An unexpected error occurred. Please try again later.";
-
-        // Define the unexpectedError variable
-        const unexpectedError = document.getElementById("Unexpected-error");
-
-        showAndHideMessage(unexpectedError, 4);
-
-        resetAllFields();
-      }
-    });
-
-    function resetAllFields() {
-      document.getElementById("email-address").value = "";
-      document.getElementById("password").value = "";
-    }
-    document.getElementById("btn-register").addEventListener("click", () => {
-      window.location.href = "./register.html"; // Redirect to register page
-    });
-  });
 
   // Update the HTML with the user's email
   const welcomeElement = document.getElementById("welcome");
@@ -178,6 +79,15 @@ document.addEventListener("DOMContentLoaded", () => {
             }
           });
 
+          // JavaScript Code Only for Mobile version
+          if (
+            /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+              navigator.userAgent
+            )
+          ) {
+            addPadding(deleteButton);
+          }
+
           updateButton.textContent = "Update";
           updateButton.classList.add("update-button");
           updateButton.addEventListener("click", function () {
@@ -192,6 +102,14 @@ document.addEventListener("DOMContentLoaded", () => {
             window.location.href = "./update.html";
             return recordId;
           });
+
+          if (
+            /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+              navigator.userAgent
+            )
+          ) {
+            addPadding(updateButton);
+          }
 
           row.appendChild(rowNumber);
           row.appendChild(date);
@@ -252,6 +170,14 @@ document.addEventListener("DOMContentLoaded", () => {
     setTimeout(() => {
       successMessage.style.display = "none";
     }, 3000);
+  }
+
+  function addPadding(element) {
+    element.style.paddingTop = "3%";
+    element.style.paddingBottom = "3%";
+    element.style.paddingLeft = "5%";
+    element.style.paddingRight = "5%";
+    return element;
   }
   document.getElementById("btn-logout").addEventListener("click", () => {
     localStorage.removeItem("token"); // Remove token from local storage
